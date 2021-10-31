a6b78942673061483639ea1799bbc97725ed9221
mistryswapnil <swapnilmistry1997@gmail.com>
2021-10-31 13:39:36 +0530

    react

--- ecommerce-firebase-react/src/Constants.js
+++ ecommerce-firebase-react/src/Constants.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyD7OuP7UphXSb4qiw0VjcbkgWFKekpFbIY",
-  authDomain: "ecommerce-bb196.firebaseapp.com",
-  projectId: "ecommerce-bb196",
-  storageBucket: "ecommerce-bb196.appspot.com",
-  messagingSenderId: "579579464842",
-  appId: "1:579579464842:web:b21e5d07c0f6a3721ef173",
+    // <YOUR FIREBASE CONFIG>
 };
 
 export {
