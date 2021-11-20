f61e4703b920798005cf1ff6c64db9a66deb385f
mistryswapnil <swapnilmistry1997@gmail.com>
2021-11-20 12:37:00 +0530

    react&redux

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -2,10 +2,10 @@ import { store } from "./redux";
 import { Provider } from "react-redux";
 
 import "./App.css";
-import Car from "./components/Car";
-import Bike from "./components/Bike";
-import CarHooks from "./components/CarHooks";
-import CarPayload from "./components/CarPayload";
+// import Car from "./components/Car";
+// import Bike from "./components/Bike";
+// import CarHooks from "./components/CarHooks";
+// import CarPayload from "./components/CarPayload";
 import Users from "./components/Users";
 
 function App() {
